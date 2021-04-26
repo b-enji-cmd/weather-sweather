50b1e15fe00788bb10b26f1ba79742e79592658f
Ben Fulton <benjamin.fulton99@gmail.com>
2021-04-26 09:47:27 -0600

    Update controller

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CitiesController < ApplicationController
     def show
     	transient_salaries = TeleportFacade.get_salaries(params[:destination])
+    	render json: SalariesSerializer.new(transient_salaries), status: 200
     end
 end
