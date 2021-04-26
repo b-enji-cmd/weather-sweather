988e2a868b114cd88cf7b5d9af1f5b417c23a1ff
Ben Fulton <benjamin.fulton99@gmail.com>
2021-04-26 10:15:32 -0600

    Update testing to reflect serializer json

--- spec/requests/city_spec.rb
+++ spec/requests/city_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe 'City API', type: :request do
 		before {get "/api/v1/salaries?destination=chicago" }
       it "returns the forecast for a city" do
       	expect(json[:data][:type]).to eq 'salaries'
-      	expect(json[:data][:attributes].keys).to include("forecast")
+      	expect(json[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
       	expect(json[:data][:attributes][:forecast].keys).to eq([:summary,:temperature])
       end
 
       it "returns salary information" do
-      	expect(json[:data][:salaries].class).to eq Array
-      	expect(json[:data][:salaries].first.keys).to eq([:title,:min,:max])
+      	expect(json[:data][:attributes][:salaries].class).to eq Array
+      	expect(json[:data][:attributes][:salaries].first.keys).to eq([:title,:min,:max])
       end
 
       it "returns the destination" do
-      	expect(json[:data][:destination]).to eq'chicago'
+      	expect(json[:data][:attributes][:destination]).to eq'chicago'
       end
 	end
 end
