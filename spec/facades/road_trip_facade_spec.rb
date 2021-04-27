03840ae05462ab62d19e662e0fcbb601e70742af
Ben <benjamin.fulton99@gmail.com>
2021-04-27 12:34:21 -0600

    Updates road trip testing for edge case and sad paths

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -25,7 +25,35 @@ RSpec.describe "Road Trip Facade" do
       trip_length = MapFacade.get_trip_length(bad_locations)
       trip_lat_long = MapFacade.get_location_details(bad_locations[:destination])
       destination_weather = WeatherFacade.get_eta_weather(trip_lat_long)
-      actual = RoadTripFacade.get_trip(locations, trip_length, destination_weather )
+      actual = RoadTripFacade.get_trip(bad_locations, trip_length, destination_weather )
+      expect(actual.class).to eq RoadTrip
+      expect(actual.travel_time).to be_nil
+      expect(actual).to respond_to(:start_city)
+      expect(actual).to respond_to(:travel_time)
+      expect(actual.weather_at_eta).to eq("Invalid Parameter")
+    end
+  end
+
+  it "should return an edge case trip" do
+    VCR.use_cassette("road_trip_facade_edge", :record => :new_episodes) do
+      trip_length = MapFacade.get_trip_length({origin: "Denver,CO"})
+      trip_lat_long = MapFacade.get_location_details(bad_locations[:destination])
+      destination_weather = WeatherFacade.get_eta_weather(trip_lat_long)
+      actual = RoadTripFacade.get_trip(bad_locations, trip_length, destination_weather )
+      expect(actual.class).to eq RoadTrip
+      expect(actual.travel_time).to be_nil
+      expect(actual).to respond_to(:start_city)
+      expect(actual).to respond_to(:travel_time)
+      expect(actual.weather_at_eta).to eq("Invalid Parameter")
+    end
+  end
+
+  it "should return an edge case trip" do
+    VCR.use_cassette("road_trip_facade_edge", :record => :new_episodes) do
+      trip_length = MapFacade.get_trip_length({destination: "Denver,CO"})
+      trip_lat_long = MapFacade.get_location_details(bad_locations[:destination])
+      destination_weather = WeatherFacade.get_eta_weather(trip_lat_long)
+      actual = RoadTripFacade.get_trip(bad_locations, trip_length, destination_weather )
       expect(actual.class).to eq RoadTrip
       expect(actual.travel_time).to be_nil
       expect(actual).to respond_to(:start_city)
