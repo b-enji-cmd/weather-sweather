bee03ce3c19c9df0d94f086b38d9d59f5cc1e975
Ben <benjamin.fulton99@gmail.com>
2021-04-26 18:53:40 -0600

    Write updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       post "/sessions", to: "sessions#create"
       get '/forecast', to: 'weather#index'
       get '/backgrounds', to: 'image#show'
+      post '/road_trip', to: 'road_trip#create'
     end
   end
 end
