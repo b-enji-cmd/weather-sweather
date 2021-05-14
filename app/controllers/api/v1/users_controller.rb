c2182ee3b1c0beea919a1311f324a1f64fd69767
Ben <benjamin.fulton99@gmail.com>
2021-05-14 13:23:18 -0600

    Updates login and registration validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+    before_action :validate_params, only: [:create]
     def create
         user = User.create!(user_params)
         user.update(api_key: SecureRandom.hex)
         json_response(UserSerializer.new(user), 201)
+    end
+
+    def validate_params
+        params[:password_confirmation].present? ? create : json_response({ message: 'Missing Password Confirmation' }, :unprocessable_entity)
     end
 
     private
